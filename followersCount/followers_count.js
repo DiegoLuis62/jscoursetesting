fd5148b028652d32ab7c07ed08a445216cc30d30
Diego <dllinero@uninorte.edu.co>
2025-08-03 14:10:14 -0400

    second lab of cousera

---
+++ followersCount/followers_count.js
@@ -0,0 +1,19 @@
+let count = 0
+function increaseCount() {
+    count++;
+    displayCount()
+    checkCountValue()
+}
+
+function displayCount() {
+    document.getElementById("countDisplay").innerHTML = count;
+}
+
+function checkCountValue() {
+    if (count === 10) {
+        alert("your intagram post gained 10 followers! congratulation!")
+    } else if (count === 20) {
+        alert("Your instagram post gained 20 follower ! keep it up!")
+
+    }
+}
